8ae5860faab2fc9fc4d39b42fa2768c0603398ac
Figus13 <figusm@hotmail.it>
2021-12-04 12:17:56 +0100

    Initial commit

---
+++ irq_timer.d
@@ -0,0 +1,8 @@
+.\irq_timer.o: timer\IRQ_timer.c
+.\irq_timer.o: C:\Program Files\Keil\ARM\Inc\NXP\LPC17xx\lpc17xx.h
+.\irq_timer.o: C:\Program Files\Keil\ARM\CMSIS\Include\core_cm3.h
+.\irq_timer.o: C:\Program Files\Keil\ARM\ARMCC\bin\..\include\stdint.h
+.\irq_timer.o: C:\Program Files\Keil\ARM\CMSIS\Include\core_cmInstr.h
+.\irq_timer.o: C:\Program Files\Keil\ARM\CMSIS\Include\core_cmFunc.h
+.\irq_timer.o: C:\Program Files\Keil\ARM\Inc\NXP\LPC17xx\system_LPC17xx.h
+.\irq_timer.o: timer\timer.h
---
+++ sample.c
@@ -0,0 +1,34 @@
+/*----------------------------------------------------------------------------
+ * Name:    sample.c
+ * Purpose: to control led through EINT buttons
+ * Note(s):
+ *----------------------------------------------------------------------------
+ *
+ * This software is supplied "AS IS" without warranties of any kind.
+ *
+ * Copyright (c) 2017 Politecnico di Torino. All rights reserved.
+ *----------------------------------------------------------------------------*/
+                  
+#include <stdio.h>
+#include "LPC17xx.H"                    /* LPC17xx definitions                */
+#include "led/led.h"
+#include "button_EXINT/button.h"
+#include "timer/timer.h"
+
+
+
+/*----------------------------------------------------------------------------
+  Main Program
+ *----------------------------------------------------------------------------*/
+int main (void) {
+  	
+	SystemInit();  												/* System Initialization (i.e., PLL)  */
+  LED_init();                           /* LED Initialization                 */
+  BUTTON_init();												/* BUTTON Initialization              */
+	init_timer(0,0x017D7840);							/* TIMER0 Initialization              */
+	enable_timer(0);				
+		
+  while (1) {                           /* Loop forever                       */	
+  }
+
+}
